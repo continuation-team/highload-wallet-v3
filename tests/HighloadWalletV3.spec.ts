2e7ec8c4c34c7e0991befafe60467dbedf95c1a6
Trinketer22 <trinketer22@localhost>
2024-03-18 06:14:46 +0300

    Highload V3 tests update

--- tests/HighloadWalletV3.spec.ts
+++ tests/HighloadWalletV3.spec.ts
@@ -1,5 +1,5 @@
-import { Blockchain, BlockchainTransaction, EmulationError, SandboxContract } from '@ton/sandbox';
-import { beginCell, Cell, SendMode, toNano } from '@ton/core';
+import { Blockchain, EmulationError, SandboxContract, createShardAccount } from '@ton/sandbox';
+import { beginCell, Cell, Address, OutActionSendMsg, SendMode, toNano, Dictionary, BitString, BitBuilder } from '@ton/core';
 import { HighloadWalletV3 } from '../wrappers/HighloadWalletV3';
 import '@ton/test-utils';
 import { getSecureRandomBytes, KeyPair, keyPairFromSeed } from "ton-crypto";
@@ -8,6 +8,7 @@ import { SUBWALLET_ID } from "./imports/const";
 import { Errors } from "./imports/const";
 import { getRandomInt } from "../utils";
 import { compile } from '@ton/blueprint';
+import { findTransactionRequired } from '@ton/test-utils';
 
 
 describe('HighloadWalletV3', () => {
@@ -17,6 +18,7 @@ describe('HighloadWalletV3', () => {
     let blockchain: Blockchain;
     let highloadWalletV3: SandboxContract<HighloadWalletV3>;
     let shouldRejectWith: (p: Promise<unknown>, code: number) => Promise<void>;
+    let getContractData: (address: Address) => Promise<Cell>;
 
     beforeAll(async () => {
         keyPair = keyPairFromSeed(await getSecureRandomBytes(32));
@@ -34,6 +36,16 @@ describe('HighloadWalletV3', () => {
                     throw e;
                 }
             }
+        }
+        getContractData = async (address: Address) => {
+          const smc = await blockchain.getContract(address);
+          if(!smc.account.account)
+            throw("Account not found")
+          if(smc.account.account.storage.state.type != "active" )
+            throw("Atempting to get data on inactive account");
+          if(!smc.account.account.storage.state.state.data)
+            throw("Data is not present");
+          return smc.account.account.storage.state.state.data
         }
     });
 
@@ -137,10 +149,11 @@ describe('HighloadWalletV3', () => {
     });
 
     it('should fail check created time', async () => {
+        const curTimeout = await highloadWalletV3.getTimeout();
         await shouldRejectWith(highloadWalletV3.sendExternalMessage(
             keyPair.secretKey,
             {
-                createdAt: 1000 - getRandomInt(100, 200),
+                createdAt: 1000 - getRandomInt(curTimeout + 1, curTimeout + 200),
                 shift: 0,
                 bitNumber: 0,
                 actions: [],
@@ -176,7 +189,41 @@ describe('HighloadWalletV3', () => {
             }
         ), Errors.already_executed)
     });
+    it('should work max bitNumber = 1022', async () => {
+        await expect(highloadWalletV3.sendExternalMessage(
+            keyPair.secretKey,
+            {
+                createdAt: 1000,
+                shift: 0,
+                bitNumber: 1022,
+                actions: [],
+                subwalletId: SUBWALLET_ID
+            })).resolves.not.toThrow(EmulationError);
+    });
 
+    it('should reject with bitNumber = 1023', async () => {
+        await expect(highloadWalletV3.sendExternalMessage(
+            keyPair.secretKey,
+            {
+                createdAt: 1000,
+                shift: 0,
+                bitNumber: 1023,
+                actions: [],
+                subwalletId: SUBWALLET_ID
+            })).rejects.toThrow(EmulationError);
+    });
+    // Just in case
+    it('should work with max shift = 16383', async () => {
+        await expect(highloadWalletV3.sendExternalMessage(
+            keyPair.secretKey,
+            {
+                createdAt: 1000,
+                shift: 16383,
+                bitNumber: 0,
+                actions: [],
+                subwalletId: SUBWALLET_ID
+            })).resolves.not.toThrow(EmulationError);
+    });
     it('should fail check query_id in old queries', async () => {
         const testResult = await highloadWalletV3.sendExternalMessage(
             keyPair.secretKey,
@@ -208,12 +255,18 @@ describe('HighloadWalletV3', () => {
     });
 
     it('should be cleared queries hashmaps', async () => {
+
+        const rndShift   = getRandomInt(0, 16383);
+        const rndBitNum  = getRandomInt(0, 1022);
+
+        const queryId = (rndShift << 10) + rndBitNum;
+
         const testResult1 = await highloadWalletV3.sendExternalMessage(
             keyPair.secretKey,
             {
                 createdAt: 1000,
-                shift: 0,
-                bitNumber: 0,
+                shift: rndShift,
+                bitNumber: rndBitNum,
                 actions: [],
                 subwalletId: SUBWALLET_ID
             }
@@ -223,14 +276,20 @@ describe('HighloadWalletV3', () => {
             success: true
         });
 
+        expect(await highloadWalletV3.getProcessed(queryId)).toBe(true);
         blockchain.now = 1000 + 260;
+
+        const newShift   = getRandomInt(0, 16383);
+        const newBitNum  = getRandomInt(0, 1022);
+
+        const newQueryId = (newShift << 10) + newBitNum;
 
         const testResult2 = await highloadWalletV3.sendExternalMessage(
             keyPair.secretKey,
             {
                 createdAt: 1200,
-                shift: 0,
-                bitNumber: 0,
+                shift: newShift,
+                bitNumber: newBitNum,
                 actions: [],
                 subwalletId: SUBWALLET_ID
             }
@@ -239,15 +298,25 @@ describe('HighloadWalletV3', () => {
             to: highloadWalletV3.address,
             success: true
         });
+        expect(await highloadWalletV3.getProcessed(queryId)).toBe(false);
+        expect(await highloadWalletV3.getProcessed(newQueryId)).toBe(true);
+        expect(await highloadWalletV3.getLastCleaned()).toEqual(testResult2.transactions[0].now);
     });
 
-    it('should send ordinary transaction', async () => {
+    it('should send ordinary transaction and set processed accordingly', async () => {
+        const testBody   = beginCell().storeUint(getRandomInt(0, 1000000), 32).endCell();
+
+        const rndShift   = getRandomInt(0, 16383);
+        const rndBitNum  = getRandomInt(0, 1022);
+
+        const queryId = (rndShift << 10) + rndBitNum;
+
         const testResult = await highloadWalletV3.sendExternalMessage(
             keyPair.secretKey,
             {
                 createdAt: 1000,
-                shift: 0,
-                bitNumber: 0,
+                shift: rndShift,
+                bitNumber: rndBitNum,
                 actions: [{
                     type: 'sendMsg',
                     mode: SendMode.NONE,
@@ -257,21 +326,121 @@ describe('HighloadWalletV3', () => {
                             createdAt: 0,
                             createdLt: 0n
                         },
-                        body: beginCell().endCell()
+                        body: testBody
                     }
                 }],
                 subwalletId: SUBWALLET_ID
             }
         );
 
-        expect(testResult.transactions).toHaveTransaction({
+        const sentTx = findTransactionRequired(testResult.transactions, {
             to: highloadWalletV3.address,
             success: true,
             outMessagesCount: 1,
-            actionResultCode: 0
+            actionResultCode: 0,
         });
-    });
+        expect(sentTx.externals.length).toBe(1);
+        expect(sentTx.externals[0].body).toEqualCell(testBody);
 
+        const processed = await highloadWalletV3.getProcessed(queryId);
+        expect(processed).toBe(true);
+    });
+    it('should handle max actions (255) in single batch', async () => {
+        const baseInt = getRandomInt(0, 100000);
+        const actions : OutActionSendMsg[] = new Array(255);
+        const rndShift  = getRandomInt(0, 16383);
+        const rndBitNum = getRandomInt(0, 1022);
+
+        const queryId = (rndShift << 10) + rndBitNum;
+        for(let i = 0; i < 255; i++) {
+            actions[i] = {
+                type: 'sendMsg',
+                mode: SendMode.NONE,
+                outMsg: {
+                    info: {
+                        type: 'external-out',
+                        createdAt: blockchain.now!,
+                        createdLt: blockchain.lt
+                    },
+                    body: beginCell().storeUint(baseInt + i, 32).endCell()
+                }
+            };
+        }
+
+        const res = await highloadWalletV3.sendExternalMessage(
+            keyPair.secretKey,
+            {
+                createdAt: 1000,
+                shift: rndShift,
+                bitNumber: rndBitNum,
+                actions,
+                subwalletId: SUBWALLET_ID
+            });
+
+        const batchTx = findTransactionRequired(res.transactions, {
+            on: highloadWalletV3.address,
+            outMessagesCount: 255
+        });
+
+        expect(batchTx.externals.length).toBe(255);
+        for(let i = 0; i < 255; i++) {
+            expect(batchTx.externals[i].body).toEqualCell(actions[i].outMsg.body);
+        }
+
+        expect(await highloadWalletV3.getProcessed(queryId)).toBe(true);
+    });
+    it('queries dictionary with max keys should fit in credit limit', async () => {
+        // 2 ** 14 - 1 = 16383 keys
+        const newQueries = Dictionary.empty(Dictionary.Keys.Uint(14), Dictionary.Values.Cell());
+
+        const padding = new BitString(Buffer.alloc(128, 0), 0, 1023 - 14);
+
+        for(let i = 0; i < 16383; i++) {
+            newQueries.set(i, beginCell().storeUint(i, 14).storeBits(padding).endCell());
+        }
+
+        const smc = await blockchain.getContract(highloadWalletV3.address);
+        const walletState = await getContractData(highloadWalletV3.address);
+        const ws   = walletState.beginParse();
+        const head = ws.loadBits(256 + 32); // pubkey + subwallet
+        const tail = ws.skip(2 + 40).loadBits(16);
+
+        const newState = beginCell()
+                          .storeBits(head)
+                          .storeDict(null)
+                          .storeDict(newQueries)
+                          .storeUint(2000, 40) // Make dictionary is not nulled
+                          .storeBits(tail)
+                        .endCell();
+
+        await blockchain.setShardAccount(highloadWalletV3.address, createShardAccount({
+            address: highloadWalletV3.address,
+            code,
+            data: newState,
+            balance: smc.balance,
+            workchain: 0
+        }));
+
+        await expect(highloadWalletV3.sendExternalMessage(
+            keyPair.secretKey,
+            {
+                createdAt: 1000,
+                shift: getRandomInt(1, 16382),
+                bitNumber: getRandomInt(0, 1022),
+                actions: [{
+                    type: 'sendMsg',
+                    mode: SendMode.NONE,
+                    outMsg: {
+                        info: {
+                            type: 'external-out',
+                            createdAt: blockchain.now!,
+                            createdLt: blockchain.lt
+                        },
+                        body: beginCell().storeUint(1234, 32).endCell(),
+                    }}],
+                subwalletId: SUBWALLET_ID
+            })).resolves.not.toThrow();
+    });
     it('should send hundred ordinary transactions', async () => {
         for (let x = 0; x < 10; x++) {
             if (x > 4) { blockchain.now = 1200; }
@@ -307,5 +476,4 @@ describe('HighloadWalletV3', () => {
               }
         }
     });
-
 });
